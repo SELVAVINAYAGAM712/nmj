1bcabd93b6b90e4303b2a6a5e44bf5620bf027eb
VinayagaM <selvavinayagam@kpost.in>
2025-02-25 21:34:56 +0530

    App.js issuse

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
-import { BrowserRouter as Router, } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import MenuRoutes from "./MenuRoutes";
 
 function App() {
   return (
-    <>
-      <Router>
-        <div>
-          {/* <Navbar /> */}
-          <MenuRoutes />
-        </div>
-      </Router>
-    </>
+    <Router>
+      <div>
+        <MenuRoutes />
+      </div>
+    </Router>
   );
 }
 
--- src/MenuRoutes.js
+++ src/MenuRoutes.js
@@ -1,26 +1,20 @@
+import { Routes, Route } from 'react-router-dom'; // ✅ Correct import
 import HomeDashboard from './Components/dashoard/HomeDashboard';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
-import AboutUs  from './Components/aboutUs-page/AboutUs';
+import AboutUs from './Components/aboutUs-page/AboutUs';
 import CustomizedJewl from './Components/customized_jewl/CustomizedJewl';
 import ContactUsPage from './Components/contact_us_pages/ContactUsPage';
 import Blog from './Components/blog/Blog';
 
-
-
 const MenuRoutes = () => {
     return (
-        <>
-            <Routes>
+        <Routes>
             <Route path="/" element={<HomeDashboard />} />
-                <Route path="/aboutUs-page" element={<AboutUs />} />
-                <Route path="/customized_jewl" element={<CustomizedJewl />} />
-                <Route path="/contact_us_pages" element={<ContactUsPage />} />
-                <Route path="/blog" element={<Blog />} />
-                
-            </Routes>
-        </>
-    )
+            <Route path="/aboutUs-page" element={<AboutUs />} />
+            <Route path="/customized_jewl" element={<CustomizedJewl />} />
+            <Route path="/contact_us_pages" element={<ContactUsPage />} />
+            <Route path="/blog" element={<Blog />} />
+        </Routes>
+    );
 }
 
 export default MenuRoutes;
-    
